f42a366820a8f20ac70127d575d7330c4f5b8893
Ratnesh Kumar <ratneshkr129@gmail.com>
2025-07-31 11:51:56 +0530

    Create String as input and displaying.cpp

---
+++ String as input and displaying.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str1[4] = "C++";
+    char str2[] = {'C','+','+','\0'};
+    char str3[4] = {'C','+','+','\0'};
+    string str4 = "C++";
+    cout<<str1<<endl;
+    cout<<str2<<endl;
+    cout<<str3<<endl;
+    cout<<str4;
+    return 0;
+}
+
+OUTPUT:
+C++
+C++
+C++
+C++
+
+=== Code Execution Successful ===
