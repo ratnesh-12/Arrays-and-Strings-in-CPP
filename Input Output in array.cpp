aba2de7d0c33c1ae04d281c7cbd0a7f746d544d6
Ratnesh Kumar <ratneshkr129@gmail.com>
2025-07-31 12:02:08 +0530

    Update Input Output in array.cpp

--- Input Output in array.cpp
+++ Input Output in array.cpp
@@ -2,19 +2,22 @@
 using namespace std;
 
 int main() {
-    int i;
-    int numbers[5]={20,30,40,50,60};
-    for (i=0; i<5; i++){
-        cout<<numbers[i]<<endl;
-    }
+    int i, numbers[5];
+
+    cout << "Enter 5 numbers: ";
+    for (i=0; i< 5; i++)
+        cin >> numbers[i];
+
+    cout << "You entered: ";
+    for (i=0; i<5; i++)
+        cout << numbers[i] << " ";
+
+    cout << endl;
     return 0;
 }
 
 OUTPUT:
-20
-30
-40
-50
-60
+Enter 5 numbers: 20 40 50 60 10
+You entered: 20 40 50 60 10 
 
 === Code Execution Successful ===
